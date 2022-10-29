e02a17f13aa74cb0d2c6b1a25576726e1c61f179
Spencer Lepine <spencerlepine26@gmail.com>
2022-10-28 17:26:55 -0700

    hello world java file

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -1,5 +1,9 @@
+package com.amigoscode;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println("YEET");
     }
 }
