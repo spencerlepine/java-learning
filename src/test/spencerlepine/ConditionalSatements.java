a5f941e10a67aae93a5695e8e05836d015859e18
Spencer Lepine <spencerlepine26@gmail.com>
2022-10-31 16:00:33 -0700

    using conditionals

--- src/test/spencerlepine/ConditionalSatements.java
+++ src/test/spencerlepine/ConditionalSatements.java
@@ -6,6 +6,16 @@ public class ConditionalSatements {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
+    
+
+    String myName = scanner.nextLine();
+    if (myName.equals("pepe")) {
+      System.out.println("YEet");
+    }
+
     double number1 = scanner.nextDouble();
+    if (number1 == 0) {
+      System.out.println("令です!");
+    }
   }
 }
